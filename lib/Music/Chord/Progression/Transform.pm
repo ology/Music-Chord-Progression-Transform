842995cbabf65dbb8ea4456650b22321c5e298b2
Gene Boggs <gene@ology.net>
2023-02-02 05:52:02 -0800

    Bump version

--- lib/Music/Chord/Progression/Transform.pm
+++ lib/Music/Chord/Progression/Transform.pm
@@ -2,7 +2,7 @@ package Music::Chord::Progression::Transform;
 
 # ABSTRACT: Generate transformed chord progressions
 
-our $VERSION = '0.0101';
+our $VERSION = '0.0102';
 
 use Moo;
 use strictures 2;
