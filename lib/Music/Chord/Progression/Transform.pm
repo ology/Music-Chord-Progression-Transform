1c97428304014567a730816f9285c6d72b0a6785
Gene Boggs <gene@ology.net>
2023-02-02 04:35:14 -0800

    Initial commit

---
+++ lib/Music/Chord/Progression/Transform.pm
@@ -0,0 +1,450 @@
+package Music::Chord::Progression::Transform;
+
+# ABSTRACT: Generate transformed chord progressions
+
+our $VERSION = '0.0100';
+
+use Moo;
+use strictures 2;
+use Algorithm::Combinatorics qw(variations);
+use Carp qw(croak);
+use Data::Dumper::Compact qw(ddc);
+use Music::NeoRiemannianTonnetz ();
+use Music::Chord::Note ();
+use Music::Chord::Namer qw(chordname);
+use lib map { "$ENV{HOME}/sandbox/$_/lib" } qw(Music-MelodicDevice-Transposition); # local author libs
+use Music::MelodicDevice::Transposition ();
+use namespace::clean;
+
+with 'Music::PitchNum';
+
+=head1 SYNOPSIS
+
+  use MIDI::Util qw(setup_score midi_format);
+  use Music::Chord::Progression::Transform ();
+
+  my $transform = Music::Chord::Progression::Transform->new;
+  my $chords = $transform->generate;
+  $chords = $transform->circular;
+
+  # render a midi file
+  my $score = setup_score();
+  $score->n('wn', midi_format(@$_)) for @$chords;
+  $score->write_score('transform.mid');
+
+=head1 DESCRIPTION
+
+The C<Music::Chord::Progression::Transform> module generates transposed
+and Neo-Riemann chord progressions.
+
+=head1 ATTRIBUTES
+
+=head2 base_note
+
+  $base_note = $transform->base_note;
+
+The initial C<isobase>, capitalized note on which the progression starts.
+
+Default: C<C>
+
+=cut
+
+has base_note => (
+    is      => 'ro',
+    isa     => sub { croak "$_[0] is not a valid note" unless $_[0] =~ /^[A-G][#b]?$/ },
+    default => sub { 'C' },
+);
+
+=head2 base_octave
+
+  $base_octave = $transform->base_octave;
+
+The initial note octave on which the progression starts.
+
+Default: C<4>
+
+=cut
+
+has base_octave => (
+    is      => 'ro',
+    isa     => sub { croak "$_[0] is not a valid octave" unless $_[0] =~ /^[1-8]$/ },
+    default => sub { 4 },
+);
+
+=head2 chord_quality
+
+  $chord_quality = $transform->chord_quality;
+
+The quality or "flavor" of the initial chord.
+
+Please see the L<Music::Chord::Note> module for a list of the known
+chords, like C<m> for "minor" or C<7> for a 7th chord, etc.
+
+Default: C<''> (major)
+
+=cut
+
+has chord_quality => (
+    is      => 'ro',
+    default => sub { '' },
+);
+
+=head2 base_chord
+
+  $base_chord = $transform->base_chord;
+
+The chord given by the B<base_note>, B<base_octave>, and the
+B<chord_quality>.
+
+=cut
+
+has base_chord => (
+    is => 'lazy',
+);
+
+sub _build_base_chord {
+    my ($self) = @_;
+    my $cn = Music::Chord::Note->new;
+    my @chord = $cn->chord_with_octave(
+        $self->base_note . $self->chord_quality,
+        $self->base_octave
+    );
+    return \@chord;
+}
+
+=head2 format
+
+  $format = $transform->format;
+
+The format of the returned results, as either named C<ISO> notes or
+C<midinum> integers.
+
+Default: C<ISO>
+
+=cut
+
+has format => (
+    is      => 'ro',
+    isa     => sub { croak "$_[0] is not a valid format" unless $_[0] =~ /^(?:ISO|midinum)$/ },
+    default => sub { 'ISO' },
+);
+
+=head2 semitones
+
+  $semitones = $transpose->semitones;
+
+The number of semitones of which a transposition transformation can be
+made.
+
+Default: C<7> (a perfect 5th)
+
+=cut
+
+has semitones => (
+    is      => 'ro',
+    isa     => sub { croak "$_[0] is not a valid number of semitones" unless $_[0] =~ /^[1-9]\d*$/ },
+    default => sub { 7 },
+);
+
+=head2 max
+
+  $max = $transform->max;
+
+The maximum number of I<circular> transformations to make.
+
+Default: C<4>
+
+=cut
+
+has max => (
+    is      => 'ro',
+    isa     => sub { croak "$_[0] is not a valid maximum" unless $_[0] =~ /^[1-9]\d*$/ },
+    default => sub { 4 },
+);
+
+=head2 allowed
+
+  $allowed = $transform->allowed;
+
+The allowed transformations. Currently this is either C<T>
+(transposition), C<N> Neo-Riemann, or both.
+
+Default: C<T N>
+
+=cut
+
+has allowed => (
+    is      => 'ro',
+    isa     => sub { croak "$_[0] is not valid" unless ref $_[0] eq 'ARRAY' },
+    default => sub { [qw(T N)] },
+);
+
+=head2 transforms
+
+  $transforms = $transform->transforms;
+
+The array-reference of C<T#> transposed and C<NRO> Neo-Riemann
+transformations that define the chord progression.
+
+The C<T#> transformations are a series of transposition operations,
+where the C<#> is a number between B<1> and the number of B<semitones>
+defined.
+
+For Neo-Riemann transformations, please see the
+L<Music::NeoRiemannianTonnetz> module for the allowed operations.
+
+Additionally the "non-transformation", C<I> is allowed to return the
+initial chord.
+
+This can also be given as an integer, which defines the number of
+random transformations to perform.
+
+Default: C<4>
+
+=cut
+
+has transforms => (
+    is      => 'ro',
+    isa     => sub { croak "$_[0] is not a valid transform" unless ref $_[0] eq 'ARRAY' || $_[0] =~ /^[1-9]\d*$/ },
+    default => sub { 4 },
+);
+
+=head2 verbose
+
+  $verbose = $transform->verbose;
+
+Show progress.
+
+Default: C<0>
+
+=cut
+
+has verbose => (
+    is      => 'ro',
+    isa     => sub { croak "$_[0] is not a boolean" unless $_[0] =~ /^[01]$/ },
+    default => sub { 0 },
+);
+
+has _nrt => (
+    is => 'lazy',
+);
+
+sub _build__nrt {
+    return Music::NeoRiemannianTonnetz->new;
+}
+
+has _mdt => (
+    is => 'lazy',
+);
+
+sub _build__mdt {
+    return Music::MelodicDevice::Transposition->new;
+}
+
+=head1 METHODS
+
+=head2 new
+
+  $transform = Music::Chord::Progression::Transform->new; # use defaults
+
+  $transform = Music::Chord::Progression::Transform->new( # override defaults
+    base_note     => 'Bb',
+    base_octave   => 5,
+    chord_quality => '7',
+    format        => 'midinum',
+    max           => 12,
+    allowed       => ['T'],
+    transforms    => [qw(I T1 T2 T3)],
+  );
+
+Create a new C<Music::Chord::Progression::Transform> object.
+
+=head2 generate
+
+  $chords = $transform->generate;
+
+Generate a I<linear> series of transformed chords.
+
+=cut
+
+sub generate {
+    my ($self) = @_;
+
+    my ($pitches, $notes) = $self->_get_pitches;
+
+    my @transforms = $self->_build_transform;
+
+    $self->_initial_conditions(@transforms) if $self->verbose;
+
+    my @generated;
+    my $i = 0;
+
+    for my $token (@transforms) {
+        $i++;
+
+        my $transformed = $self->_build_chord($token, $pitches, $notes);
+
+        my @notes = map { $self->pitchname($_) } @$transformed;
+        my @base = map { s/^([A-G][#b]?)\d/$1/r } @notes; # for chord-name
+
+        push @generated, $self->format eq 'ISO' ? \@notes : $transformed;
+
+        printf "%d. %s: %s   %s   %s\n",
+            $i, $token,
+            ddc($transformed), ddc(\@notes),
+            scalar chordname(@base)
+            if $self->verbose;
+
+        $notes = $transformed;
+    }
+
+    return \@generated;
+}
+
+=head2 circular
+
+  $chords = $transform->circular;
+
+Generate a series of transformed chords based on a I<circular> list of
+transformations.
+
+This method defines movement over a circular list ("necklace") of
+chord transformations, including C<I> which means to "make no
+transformation." Starting at position zero, move forward or backward
+along the necklace, transforming the current chord.
+
+=cut
+
+sub circular {
+    my ($self) = @_;
+
+    my ($pitches, $notes) = $self->_get_pitches;
+
+    my @transforms = $self->_build_transform;
+
+    $self->_initial_conditions(@transforms) if $self->verbose;
+
+    my @generated;
+    my $posn = 0;
+
+    for my $i (1 .. $self->max) {
+        my $token = $transforms[ $posn % @transforms ];
+
+        my $transformed = $self->_build_chord($token, $pitches, $notes);
+
+        my @notes = map { $self->pitchname($_) } @$transformed;
+        my @base = map { s/^([A-G][#b]?)\d/$1/r } @notes; # for chord-name
+
+        push @generated, $self->format eq 'ISO' ? \@notes : $transformed;
+
+        printf "%d. %s (%d): %s   %s   %s\n",
+            $i, $token, $posn % @transforms,
+            ddc($transformed), ddc(\@notes),
+            scalar chordname(@base)
+            if $self->verbose;
+
+        $notes = $transformed;
+
+        $posn = int rand 2 ? $posn + 1 : $posn - 1;
+    }
+
+    return \@generated;
+}
+
+sub _get_pitches {
+    my ($self) = @_;
+    my @pitches = map { $self->pitchnum($_) } @{ $self->base_chord };
+    return \@pitches, [ @pitches ];
+}
+
+sub _initial_conditions {
+    my ($self, @transforms) = @_;
+    printf "Initial: %s%s %s\nTransforms: %s\n",
+        $self->base_note, $self->base_octave, $self->chord_quality,
+        join(',', @transforms);
+}
+
+sub _build_transform {
+    my ($self) = @_;
+
+    my @t; # the transformations to return
+
+    if (ref $self->transforms eq 'ARRAY') {
+        @t = @{ $self->transforms };
+    }
+    elsif ($self->transforms =~ /^\d+$/) {
+        my @transforms;
+
+        if (grep { $_ eq 'T' } @{ $self->allowed }) {
+            push @transforms, (map { 'T' . $_ } 1 .. $self->semitones);
+        }
+        if (grep { $_ eq 'N' } @{ $self->allowed }) {
+            my @alphabet = qw(P R L);
+            push @transforms, @alphabet;
+
+            my $iter = variations(\@alphabet, 2);
+            while (my $v = $iter->next) {
+                push @transforms, join('', @$v);
+            }
+
+            $iter = variations(\@alphabet, 3);
+            while (my $v = $iter->next) {
+                push @transforms, join('', @$v);
+            }
+        }
+
+        @t = ('I',
+            map { $transforms[ int rand @transforms ] }
+                1 .. $self->transforms - 1
+        );
+    }
+
+    return @t;
+}
+
+sub _build_chord {
+    my ($self, $token, $pitches, $notes) = @_;
+
+    my $chord;
+
+    if ($token =~ /^I$/) {
+        $chord = $pitches; # no transformation
+    }
+    elsif ($token =~ /^T(-?\d+)$/) {
+        my $semitones = $1;
+        $chord = $self->_mdt->transpose($semitones, $notes);
+    }
+    else {
+        my $task = $self->_nrt->taskify_tokens($token) if length $token > 1;
+        my $op = defined $task ? $task : $token;
+
+        $chord = $self->_nrt->transform($op, $notes);
+    }
+
+    return $chord;
+}
+
+1;
+__END__
+
+=head1 SEE ALSO
+
+The F<t/01-methods.t> and F<eg/*> files
+
+L<Carp>
+
+L<Data::Dumper::Compact>
+
+L<Moo>
+
+L<Music::MelodicDevice::Transposition>
+
+L<Music::NeoRiemannianTonnetz>
+
+L<Music::Chord::Note>
+
+L<Music::Chord::Namer>
+
+L<https://viva.pressbooks.pub/openmusictheory/chapter/neo-riemannian-triadic-progressions/>
+
+=cut
---
+++ t/01-methods.t
@@ -0,0 +1,95 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Test::More;
+use Test::Exception;
+
+my $module = 'Music::Chord::Progression::Transform';
+
+use_ok $module;
+
+subtest throws => sub {
+    throws_ok { $module->new(base_note => 'I') }
+        qr/not a valid note/, 'bogus base_note';
+
+    throws_ok { $module->new(base_octave => -1) }
+        qr/not a valid octave/, 'bogus base_octave';
+
+    throws_ok { $module->new(format => 'foo') }
+        qr/not a valid format/, 'bogus format';
+
+    throws_ok { $module->new(max => -1) }
+        qr/not a valid maximum/, 'bogus max';
+
+    throws_ok { $module->new(transforms => 'foo') }
+        qr/not a valid transform/, 'bogus transform';
+
+    throws_ok { $module->new(transforms => {}) }
+        qr/not a valid transform/, 'bogus transform';
+};
+
+subtest default => sub {
+    my $obj = new_ok $module;
+    my $got = $obj->generate;
+    my $expect = 4;
+    is @$got, $expect, "generated $expect chords";
+    $expect = [qw(C4 E4 G4)];
+    is_deeply $got->[0], $expect, 'generated 0th chord';
+};
+
+subtest transform_array => sub {
+    my $obj = new_ok $module => [
+        transforms => [qw(I P T2)],
+    ];
+    my $got = $obj->generate;
+    no warnings qw(qw);
+    my $expect = [[qw(C4 E4 G4)], [qw(C4 D#4 G4)], [qw(D4 F4 A4)]];
+    is_deeply $got, $expect, 'generated chords';
+};
+
+subtest transform_integer => sub {
+    my $expect = 3;
+    my $obj = new_ok $module => [
+        transforms => $expect,
+    ];
+    my $got = $obj->generate;
+    is @$got, $expect, "generated $expect chords";
+    $expect = [qw(C4 E4 G4)];
+    is_deeply $got->[0], $expect, 'generated 0th chord';
+};
+
+subtest transform_base => sub {
+    my $obj = new_ok $module => [
+        base_note   => 'G',
+        base_octave => 5,
+    ];
+    my $got = $obj->generate;
+    my $expect = 4;
+    is @$got, $expect, "generated $expect chords";
+    $expect = [qw(G5 B5 D6)];
+    is_deeply $got->[0], $expect, 'generated 0th chord';
+};
+
+subtest midinum_format => sub {
+    my $obj = new_ok $module => [
+        format => 'midinum',
+    ];
+    my $got = $obj->generate;
+    my $expect = [qw(60 64 67)];
+    is_deeply $got->[0], $expect, 'generated 0th chord';
+};
+
+subtest circular => sub {
+    my $expect = 4;
+    my $obj = new_ok $module => [
+        transforms => [qw(I P T2)],
+        max        => $expect,
+    ];
+    my $got = $obj->circular;
+    is @$got, $expect, "generated $expect chords";
+    $expect = [qw(C4 E4 G4)];
+    is_deeply $got->[0], $expect, 'generated 0th chord';
+};
+
+done_testing();
