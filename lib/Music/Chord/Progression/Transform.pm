4bd23a4f0fdb3bd3df3f5a96f67305af01e52544
Gene Boggs <gene@ology.net>
2023-02-06 17:43:56 -0800

    -6 is a b6 chord

--- lib/Music/Chord/Progression/Transform.pm
+++ lib/Music/Chord/Progression/Transform.pm
@@ -306,7 +306,6 @@ sub generate {
 
         my $chord = chordname(@base);
         $chord =~ s/\s+//;
-        $chord =~ s/-6/6/;
         $chord =~ s/o/dim/;
         $chord = $1 . $2 if $chord =~ /^(.+)\/(\d+)$/;
         push @chords, $chord;
